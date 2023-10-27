bad9ce3a70bf9a9ee078efc7857c3fe3d98b6eaf
novitaaams <novitaaams@gmail.com>
2023-10-27 11:47:36 +0700

    disuruh surya

--- booketList/settings.py
+++ booketList/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-v#e53z_g###*x1i^@ous24d8x&$ayv1&24ilber2pxwv2!jtfo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'manajemen_buku'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'booketList.urls'
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'booketList.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ manajemen_buku/views.py
@@ -0,0 +1,127 @@
+import datetime
+from django.http import HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponse
+from django.core import serializers
+from django.urls import reverse
+from django.shortcuts import render
+from django.core import serializers
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages  
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseNotFound
+from manajemen_buku.models import Books
+
+
+# Create your views here.
+@login_required(login_url='/manajemen_buku/login')
+def manajemen_buku(request):
+    context = {
+        'nama': request.user.username, # Nama kamu
+        'products': Books.objects.filter(user=request.user)
+    }
+
+    return render(request, "manajemen_buku.html", context)
+
+def show_xml(request):
+    data = Books.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Books.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Books.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Books.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('manajemen_buku:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("manajemen_buku:manajemen_buku")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+def increment_amount(request, item_id):
+    books = Books.objects.get(pk=item_id)
+    books.halaman += 1
+    books.save()
+    return redirect('main:show_main')
+
+def decrement_amount(request, item_id):
+    books = Books.objects.get(pk=item_id)
+    if books.halaman > 0:
+        books.halaman -= 1
+        books.save()
+    if books.halaman == 0:
+        books.delete()
+    return redirect('main:show_main')
+
+def delete_books(request, item_id):
+    books = Books.objects.get(pk=item_id)
+    books.delete()
+    return redirect('main:show_main')
+
+def get_books_json(request):
+    product_item = Books.objects.all()
+    return HttpResponse(serializers.serialize('json', product_item))
+
+
+@csrf_exempt
+def add_books_ajax(request):
+    if request.method == 'POST':
+        judul = request.POST.get("judul")
+        halaman = request.POST.get("halaman")
+        description = request.POST.get("description")
+        user = request.user
+        image = request.FILES.get('image')
+
+
+        new_item = Books(judul=judul, halaman=halaman, description=description, user=user, image=image)
+        new_item.save()
+
+        return HttpResponse("CREATED", status=201)
+
+    return HttpResponseNotFound()
+
+@csrf_exempt
+def delete_books_ajax(request, item_id):
+    if request.method == 'DELETE':
+        books = Books.objects.get(id=item_id)
+        books.delete()
+        return HttpResponse({'status': 'DELETED'}, status=200)
