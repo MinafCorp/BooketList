18e9ee13864edb5ccdbeb22ccdb0ab00c10274e6
novitaaams <novitaaams@gmail.com>
2023-10-27 17:43:16 +0700

    apa ajaa bebaas kata surya kalau commit mah

--- manajemen_buku/views.py
+++ manajemen_buku/views.py
@@ -1,18 +1,17 @@
-import datetime
 from django.http import HttpResponseNotFound, HttpResponseRedirect
 from django.http import HttpResponse
 from django.core import serializers
-from django.urls import reverse
+# from django.urls import reverse
 from django.shortcuts import render
 from django.core import serializers
 from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages  
-from django.contrib.auth import authenticate, login
-from django.contrib.auth import logout
+# from django.contrib.auth import authenticate, login
+# from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+# from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseNotFound
 from book.models import Book
