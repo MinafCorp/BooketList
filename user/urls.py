e82f849bbd9ce48bf90d72c52e4947327d605bcc
farrelsheva <farrelshevaa@gmail.com>
2023-10-27 15:15:00 +0700

    updated user

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from user.views import show_landing, signup, login
+from user.views import show_landing, reader_register, author_register, login, logout
 
 app_name = 'user'
 
 urlpatterns = [
     path('', show_landing, name='show_landing'),
-    path('signup/', signup, name='signup'),
+    path('reader_register/', reader_register, name='reader_register'),
+    path('author_register/', author_register, name='author_register'),
+    path('logout/', logout, name='logout'),
     path('login/', login, name='login'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from user.forms import UserCreationForm
+from user.forms import ReaderSignUpForm, AuthorSignUpForm
 import datetime
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse, HttpResponseNotFound
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.shortcuts import redirect
@@ -15,40 +15,47 @@ from .models import User, Author, Reader
 def show_landing(request):
     return render(request, 'landing.html')
 
-def signup(request):
-    form = UserCreationForm()
-
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
+def reader_register(request):
+    form = ReaderSignUpForm()
+    
+    if request.method == 'POST':
+        form = ReaderSignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            selected_role = request.POST.get('role')
-            if selected_role == 'author':
-                author = Author(user=user)
-                author.save()
-                user.is_author = True
-                user.save()
-            elif selected_role == 'reader':
-                reader = Reader(user=user)
-                reader.save()
-                user.is_reader = True
-                user.save()
-            return redirect('user:login')
-    context = {'form':form}
-    return render(request, 'signup.html', context)
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context =  {'form': form}
+    return render(request, 'signup_reader.html', context)
+
+def author_register(request):
+    form = AuthorSignUpForm()
+    
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+        
+    context =  {'form': form}
+    return render(request, 'signup_author.html', context)
+    
 
 def login(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        role = request.POST.get('role')
-        user = authenticate(request, username=username, password=password, role=role)
-        if user is not None:
-            login(request, user)
-            response = HttpResponseRedirect(reverse("main:show_main")) 
-            response.set_cookie('last_login', str(datetime.datetime.now()))
-            return response
-        else:
-            messages.info(request, 'Incorrect username or password. Please try again.')
-    context = {}
-    return render(request, 'login.html', context)
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request,user)
+                if user.is_author:
+                    return(redirect("manajamen_buku: manajemen_buku."))
+                else:
+                    return(redirect("something"))
+        
+    return render(request, 'login.html', context={'form': AuthenticationForm()})
+
+def logout(request):
+    logout(request)
+    return redirect('login')
