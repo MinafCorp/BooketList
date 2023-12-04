5476786f4519946d2dea157dd271e7dd9c844bec
farrelsheva <farrelshevaa@gmail.com>
2023-12-04 13:57:28 +0700

    unfinished

---
+++ authentication/views.py
@@ -0,0 +1,80 @@
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+from user.forms import ReaderSignUpForm, AuthorSignUpForm
+
+@csrf_exempt
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            # NEED TO ADD ROLE VERIFICATION (OR JUST NAVIGATE TO DIFFERENT PAGE) 
+            auth_login(request, user)
+            return JsonResponse({
+                "username": user.username,
+                "status": True,
+                "message": "Login sukses!",
+                "role" : user.role,
+            }, status=200)
+        else:
+            return JsonResponse({
+                "status": False,
+                "message": "Login gagal, akun nonaktif."
+            }, status=401)
+
+    else:
+        return JsonResponse({
+            "status": False,
+            "message": "Login gagal,periksa kembali username dan password anda."
+        }, status=401)
+        
+        
+@csrf_exempt
+def logout(request):
+    username = request.user.username
+    try:
+        auth_logout(request)
+        return JsonResponse({
+            "username": username,
+            "status": True,
+            "message": "Logout berhasil!"
+        }, status=200)
+    except:
+        return JsonResponse({
+        "status": False,
+        "message": "Logout gagal."
+        }, status=401)
+     
+     
+@csrf_exempt
+@require_http_methods(["POST"])
+def register(request):
+    data = request.POST
+    role = data.get('role', '').upper()
+    form = None 
+    
+    if role == 'READER':
+        form = ReaderSignUpForm(data)
+    elif role == 'AUTHOR':
+        form = AuthorSignUpForm(data)
+    else:
+        return JsonResponse({
+            "status": False,
+            "message": "Role tidak valid/tidak ada."
+        }, status=400)
+        
+    if form and form.is_valid():
+        user = form.save()
+        return JsonResponse({
+            "username": user.username,
+            "status": True,
+            "message": "Register berhasil!"
+        }, status=200)
+    else:
+        return JsonResponse({
+            "status": False,
+            "message": "Register gagal."
+        }, status=400)
