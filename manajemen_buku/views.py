2a2302e04886f278ed67276a148fad716a898def
suryata <madeputra415@gmail.com>
2023-12-17 17:37:03 +0700

    perbaiki punya novita

--- manajemen_buku/views.py
+++ manajemen_buku/views.py
@@ -1,5 +1,7 @@
 import datetime
-from django.http import HttpResponseNotFound, HttpResponseRedirect
+import json
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponseNotFound, HttpResponseRedirect, JsonResponse
 from django.http import HttpResponse
 from django.core import serializers
 from django.urls import reverse
@@ -16,7 +18,6 @@ from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseNotFound
 from book.models import Book
-from updates.models import Updates
 from manajemen_buku.models import Publish
 from user.models import Author, Reader
 
@@ -75,10 +76,6 @@ def add_books_ajax(request):
         new_item = Book(ISBN=ISBN, title=title, author=author, year_of_publication=year_of_publication, publisher=publisher, image_url_s=image_url_s, image_url_m=image_url_m, image_url_l=image_url_l, authorUser=authorUser, image=image)
         new_item.save()
 
-        content = "new published book!"
-        new_update = Updates(title=title, content=content, author=author, author_username=authorUser.user.username)
-        new_update.save()
-
         return HttpResponse(b"CREATED", status=201)
 
     return HttpResponseNotFound()
@@ -89,10 +86,64 @@ def delete_books_ajax(request, item_id):
         books = Book.objects.get(id=item_id)
         books.delete()
         return HttpResponse({'status': 'DELETED'}, status=200)
-    
+
+@csrf_exempt
+def delete_author_book(request, book_id):
+    if request.method == 'POST':
+        try:
+            books = Book.objects.get(id=book_id)
+            books.delete()
+            
+            return JsonResponse({"success": True, "message": "Berhasil dihapus dari wishlist"}, status=200)
+        except ObjectDoesNotExist:
+            return JsonResponse({'status': 'error', 'message': 'Object not found'}, status=404)
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=400)
 
 def hide_books_ajax(request, item_id):
     if request.method == 'DELETE':
         books = Publish.objects.get(id=item_id)
         books.delete()
         return HttpResponse({'status': 'DELETED'}, status=200)
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+        user_id = request.user.id
+        print(f"User ID: {user_id}")
+        #print(data)
+        #print(type(data["ISBN"]))
+
+        new_product = Book.objects.create(
+            ISBN = int(data["ISBN"]),
+            title = data["title"],
+            author=user_id,
+            year_of_publication = int(data["YearOfPublication"]),
+            publisher = data["publisher"],
+            image_url_s = "",
+            image_url_m = "",
+            image_url_l = "",
+            image = "",
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+    
+def show_json(request):
+    data = Book.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def delete_book_by_isbn(request, isbn):
+    print(isbn)
+    try:
+        book = Book.objects.get(ISBN=isbn)
+        book.delete()
+        return JsonResponse({'status': 'success', 'message': 'Book deleted successfully'})
+    except Book.DoesNotExist:
+        return JsonResponse({'status': 'error', 'message': 'Book not found'}, status=404)
